a104f4c5e13b28046d04b868570207c353181f79
sarthakpandey001 <70168331+sarthakpandey001@users.noreply.github.com>
2021-04-21 23:52:59 +0530

    product list with db

--- app.js
+++ app.js
@@ -3,17 +3,29 @@ const mongoose = require("mongoose");//database
 const session = require("express-session");///cokkies
 const passport = require("passport");//for login and session cokkies
 const findOrCreate = require("mongoose-findorcreate");
+const morgan = require('morgan');//middleware
 //app.use(passport.initialize());
 //app.use(passport.session());
+
 
 
 var app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 const port = 3000;
+
+const dbURI= 'mongodb+srv://Devhaste:devhasteuser4321@farmfriend.tlyjg.mongodb.net/Userinfo?retryWrites=true&w=majority';
+mongoose.connect(dbURI,{ useNewUrlParser: true, useUnifiedTopology: true })
+ .then((result)=>console.log('connected to db'))
+ .catch((err) => console.log(err));
+
+
 
 app.get('/', (req, res) => {
     res.render("index",{});
+   res.redirect('/idpass');
   
 })
 
@@ -21,6 +33,31 @@ app.get('/fqc', (req, res) => {
   res.render("fqc",{});
 
 })
+
+
+app.get('/idpass', (req, res) => {
+  Idpass.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render("index", { idpass: result, title: 'Product List' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.post('/idpass', (req, res) => {
+  // console.log(req.body);
+  const idpass = new Idpass(req.body);
+
+  idpass.save()
+    .then(result => {
+      res.redirect("idpass");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
 
 
 app.get('/login',(req,res) =>{
@@ -35,6 +72,16 @@ app.get('/plist', (req, res) => {
   res.render("plist",{});
 
 })
+
+app.get('/product', (req, res) => {
+  res.render("product",{});
+
+})
+
+app.post('/talk', (req, res) => {
+  res.render("talk",{});
+})
+
 
 
   
---
+++ idpass.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const passport = require('passport');
+const Schema = mongoose.Schema;
+
+const IdSchema = new Schema({
+    User: {
+        type: String,
+        required: true,
+      },
+    username: {
+    type: String,
+    required: true,
+  },
+    email: {
+    type: email,
+    required: true,
+  },
+    password: {
+    type: password,
+    required: true
+  },
+  Productname: {
+    type: String,
+  },
+  district: {
+    type: String,
+    
+  },
+  rating: {
+    type: number,
+  },
+  Variety: {
+    type: String,
+    
+  },
+  Price: {
+    type: number,
+  },
+
+}, { timestamps: true });
+
+const Idpass = mongoose.model('Idpass', Idschema);
+module.exports = Idpass;
